6c7f694d7408a0bb87a02a6472f905e927357897
Florentin menage <fmenage@hyvibe.audio>
2025-02-26 11:53:38 +0100

    ajout du text2speech et choix de subset

--- carte_flash.py
+++ carte_flash.py
@@ -12,7 +12,57 @@ import time
 import os
 import csv
 import pandas as pd
+import socket
+import pyttsx3
+import platform
+from gtts import gTTS
 
+def is_connected():
+    """Vérifie si l'ordinateur est connecté à Internet."""
+    try:
+        # Essayer de se connecter à un serveur (par exemple Google)
+        socket.create_connection(("www.google.com", 80), timeout=2)
+        return True
+    except (socket.timeout, socket.gaierror):
+        return False
+    
+def speak(text, lang="fr"):
+    """Convertit un texte en parole et le joue sur macOS ou Windows."""
+    
+    #Si on est connecté à internet on passe par gTTs de google
+    if(is_connected()):
+        tts = gTTS(text=text, lang=lang)
+        filename = "temp.mp3"
+        tts.save(filename)
+    
+        # Détecter l'OS et utiliser la bonne commande
+        if platform.system() == "Darwin":  # macOS
+            os.system(f"afplay {filename}")
+        elif platform.system() == "Windows":
+            os.system(f"start {filename}")  # Utilise le lecteur par défaut sous Windows
+        
+        #os.remove(filename)  # Supprimer le fichier après lecture
+        
+    #Sinon on passe par un moteur interne pyttsx3
+    else:
+        engine = pyttsx3.init()
+
+        # Liste les voix disponibles
+        voices = engine.getProperty('voices')
+        
+        # Choisir la voix correspondant à la langue
+        for voice in voices:
+            # Pour le français
+            if lang in voice.languages[0]:
+                engine.setProperty('voice', voice.id)
+                break
+            
+        # Si hors ligne, utiliser pyttsx3 pour la lecture locale
+        engine.setProperty('rate', 150)  # Ajuste la vitesse si nécessaire
+        engine.setProperty('volume', 1)  # Volume max
+        engine.say(text)
+        engine.runAndWait()
+        
 class Flashcard:
     def __init__(self, ID, question, answer, difficulty):
         self.ID = ID
@@ -44,6 +94,8 @@ class Flashcards:
         self.cards = []
         self.finished_cards = []
         self.is_swapped = False
+        self.langue_pile = None
+        self.langue_face = None
 
     def add_card(self, ID, question, answer, difficulty):
         card = Flashcard(ID, question, answer, difficulty)
@@ -72,11 +124,9 @@ class Flashcards:
     def get_nb_mots_restants(self):
         return len(self.cards)
     
-    def random_extraction(self, N):
+    def random_extraction(self, N, prop_easy=0.1, prop_medium = 0.1, prop_hard = 0.8):
         #On va extraire du jeu les cartes mais avec une proportion qui varie selon la difficulité
-        prop_easy = 0.1
-        prop_medium = 0.1
-        prop_hard = 0.8
+
         N0_voulu = int(prop_easy*N)
         N1_voulu = int(prop_medium*N)
         N2_voulu = int(prop_hard*N)
@@ -146,22 +196,23 @@ class FlashcardsApp:
         self.answer_entry = tk.Entry(master, font=medium_font, width=30)
         self.answer_entry.pack(pady=15, ipady=10)
 
-        self.show_answer_button = tk.Button(master, text="Afficher la réponse", command=self.show_answer, font=medium_font)
+        self.show_answer_button = tk.Button(master, text="👁 Afficher la réponse", command=self.show_answer, font=medium_font)
         self.show_answer_button.pack(pady=15, ipadx=20, ipady=10)
 
+        #------
         self.response_label = tk.Label(master, text="", font=medium_font, fg="blue")
         self.response_label.pack(pady=15)
 
         button_frame = tk.Frame(master)
         button_frame.pack(pady=15)
 
-        self.correct_button = tk.Button(button_frame, text="✔ Correct", command=self.on_correct_click, font=medium_font, bg="lightgreen", width=10)
+        self.correct_button = tk.Button(button_frame, text="✅ Correct", command=self.on_correct_click, font=medium_font, bg="lightgreen", width=10)
         self.correct_button.pack(side=tk.LEFT, padx=20, ipadx=10, ipady=10)
 
-        self.faux_button = tk.Button(button_frame, text="✘ Faux", command=self.on_faux_click, font=medium_font, bg="lightcoral", width=10)
+        self.faux_button = tk.Button(button_frame, text="❌ Faux", command=self.on_faux_click, font=medium_font, bg="lightcoral", width=10)
         self.faux_button.pack(side=tk.LEFT, padx=20, ipadx=10, ipady=10)
 
-        self.swap_button = tk.Button(master, text="Inverser Question/Définition", command=self.flashcards.swap_all_sides, font=medium_font)
+        self.swap_button = tk.Button(master, text="↔ Inverser Question/Définition", command=self.flashcards.swap_all_sides, font=medium_font)
         self.swap_button.pack(pady=30, ipadx=20, ipady=10)
 
         self.score_label = tk.Label(master, text="", font=small_font, fg="gray")
@@ -174,10 +225,18 @@ class FlashcardsApp:
         self.timer_label.pack(pady=15)
         self.update_timer()
         
+        self.speaker_button = None
+
         self.load_next_card()
         self.master.bind('<Return>', self.handle_enter_key)
+        
 
     def load_next_card(self):
+        
+        if self.speaker_button is not None:
+            self.speaker_button.pack_forget()
+            self.speaker_button = None
+        
         self.current_card = self.flashcards.get_first_card()
         
         if not self.current_card:
@@ -228,7 +287,18 @@ class FlashcardsApp:
                 self.response_label.config(text=self.current_card.answer)
             else:
                 self.response_label.config(text=self.current_card.question)
+        
+        if not self.speaker_button:
+            self.speaker_button = tk.Button(self.master, text="🔊", font=("Helvetica", 20), command = self.speak_current_answer)
+            self.speaker_button.pack(pady=15, ipadx=10, ipady=10)
 
+    def speak_current_answer(self):
+        if self.current_card:
+            if(self.current_card.is_swapped):
+                speak(self.current_card.answer,lang=self.flashcards.langue_face)
+            else:
+                speak(self.current_card.question,lang=self.flashcards.langue_pile)
+        
     def update_timer(self):
         elapsed_time = int(time.time() - self.start_time)
         hours, remainder = divmod(elapsed_time, 3600)
@@ -256,20 +326,46 @@ class FlashcardsApp:
 
 
             
-def read_flashcards_from_csv(file_path_csv, taille_jeu):
+def read_flashcards_from_csv(file_path_csv, taille_jeu, prop_easy, prop_medium, prop_hard, subset_start = None, subset_stop = None):
+    
+    #Le jeu avec lequel on va jouer
     flashcards = Flashcards()
 
-    df = pd.read_csv(file_path_csv, sep=";")
+    #Par défaut, on met tout en français
+    langue_pile = "fr"
+    langue_face = "fr"
+    
+    #On lit les métadonnées s'il y en a 
+    with open(file_path_csv, "r") as f:
+        for _ in range(10):  #On lit les métadonnées dans les 10 premières lignes
+            line = f.readline()
+            if line.startswith("#langue_pile"):
+                langue_pile = line.split('=')[1].strip().strip('"')
+            elif line.startswith("#langue_face"):
+                langue_face = line.split('=')[1].strip().strip('"')
+    
+    flashcards.langue_pile = langue_pile
+    flashcards.langue_face = langue_face
+    
+    #On lit le contenu
+    df = pd.read_csv(file_path_csv, sep=";", comment = "#")
     
     for index, row in df.iterrows():
-        flashcards.add_card(index, row["Pile"], row["Face"], row["Difficulté"])
+        #On selectionne le subset : par exemple les cartes de 0 à 100
+        if( subset_start != None and subset_stop != None):
+            if(index>= subset_start and index< subset_stop):
+                flashcards.add_card(index, row["Pile"], row["Face"], row["Difficulté"])
+        else:
+            flashcards.add_card(index, row["Pile"], row["Face"], row["Difficulté"])
             
     if(flashcards.get_nb_mots_restants()>taille_jeu):
-        flashcards.random_extraction(taille_jeu)
+        #On extrait le nombre de cartes demandé
+        flashcards.random_extraction(taille_jeu, prop_easy, prop_medium, prop_hard)
     else:
+        #Si le nb de cartes demandé est inférieur au nb de cartes disponibles on mélange juste le jeu
         flashcards.shuffle()
 
-
+    
     return flashcards, df
 
 def get_infos_from_csv(file_path_csv):
@@ -325,9 +421,9 @@ def merge_two_csv(filepath_1, filepath_2):
     df_tot.drop_duplicates(subset=['Pile'], keep = "first")
     df_tot.to_csv(filepath_1, index=False)
     
-def main(file_path_csv, swap=0, nb_cartes=20):
+def main(file_path_csv, swap=0, nb_cartes=20, subset_start=None, subset_stop=None, prop_easy=0.1, prop_medium=0.1, prop_hard=0.8):
 
-    flashcards, df = read_flashcards_from_csv(file_path_csv, nb_cartes)
+    flashcards, df = read_flashcards_from_csv(file_path_csv, nb_cartes, prop_easy, prop_medium, prop_hard, subset_start, subset_stop)
     if(swap):
         flashcards.swap_all_sides()
         
@@ -343,10 +439,28 @@ def main(file_path_csv, swap=0, nb_cartes=20):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Jeu de flashcards')
-    parser.add_argument('--fp', metavar='', required=True,
-                        help='path to dico csv')
+    parser.add_argument('--fp', default = "dico/test.csv", metavar='',help='path to dico csv')
     parser.add_argument('--swap', default = 0, type=int, help="swap ou pas les questions")
     parser.add_argument('--nb_cartes', default=20, type=int, help="nb de cartes")
+    parser.add_argument('--subset_start', default=0, type=int, help="debut du subet")
+    parser.add_argument('--subset_stop', default=20, type=int, help="fin du subset")
+    parser.add_argument('--prop_easy',  default = 0.1, type=float, help = "proportion de cartes faciles")
+    parser.add_argument('--prop_medium',  default = 0.1, type=float, help = "proportion de cartes faciles")
+    parser.add_argument('--prop_hard',  default = 0.8, type=float, help = "proportion de cartes faciles")
+
     args = parser.parse_args()
+
+    if( (args.subset_start != None) and (args.subset_stop != None) ):
+        if(args.subset_start > args.subset_stop):
+            print("Error : subset_start > subset_stop. Ignoring subsets")
+            args.subset_start = None
+            args.subset_stop = None
     
-    main(args.fp, swap=args.swap, nb_cartes=args.nb_cartes)
+    if(args.prop_easy + args.prop_medium + args.prop_hard != 1.0):
+        raise ValueError("Somme des proportions de diffulté différent de 1")
+        args.prop_easy = 0.1
+        args.prop_medium = 0.1
+        args.prop_hard = 0.8
+
+    
+    main(args.fp, swap=args.swap, nb_cartes=args.nb_cartes, subset_start=args.subset_start, subset_stop=args.subset_stop, prop_easy = args.prop_easy, prop_medium = args.prop_medium, prop_hard = args.prop_hard)
